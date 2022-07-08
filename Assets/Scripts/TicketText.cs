b941b322f7762c90803564c004c6392541fd7c7c
GiovannyBrugge <giovannybrugge@live.nl>
2022-07-08 09:58:01 +0200

    Added tickets text
    Added tickets text + script

---
+++ Assets/Scripts/TicketText.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+
+public class TicketText : MonoBehaviour
+{
+    public TMP_Text ticketTimeText;
+
+    private void Start()
+    {
+        ticketTimeText.text = "12:00";
+    }
+}
